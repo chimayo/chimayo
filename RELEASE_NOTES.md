d3be3f29cebe32454a53bbb53481596628616b1e
Steve Horsfield <stevehorsfield@users.noreply.github.com>
2016-08-06 07:23:38 +0200

    Bump version to 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,8 @@
+### 0.0.2 - August 6 2016
+
+* Basic documentation
+* NuGet package fix for missing config file for command line tool
+
 ### 0.0.1 Initial release
 
 * Initial release
--- src/Chimayo.Ssis.Reader2012/AssemblyInfo.fs
+++ src/Chimayo.Ssis.Reader2012/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Chimayo.Ssis.Reader2012")>]
 [<assembly: AssemblyProductAttribute("Chimayo.Ssis")>]
 [<assembly: AssemblyDescriptionAttribute("Chimayo is a library for creating and manipulating SQL Server Integration Services packages with F# code.")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
-    let [<Literal>] InformationalVersion = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] InformationalVersion = "0.0.2"
